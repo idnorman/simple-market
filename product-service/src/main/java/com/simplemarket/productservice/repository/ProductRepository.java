e25ce13b2132538000f0ea2bba9deb87aed6decc
Norman <norman.primary@gmail.com>
2023-02-03 04:11:06 +0700

    update .gitignore and dockerize app

--- product-service/src/main/java/com/simplemarket/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.simplemarket.productservice.repository;
-
-import com.simplemarket.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
